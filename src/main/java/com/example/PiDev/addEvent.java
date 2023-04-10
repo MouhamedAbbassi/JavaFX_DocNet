0f6a41f6fc43cc63ed50fff478a17560172d058d
semah <sameh.naski@esprit.tn>
2023-04-10 11:35:40 +0100

    Controle de saisie

--- src/main/java/com/example/PiDev/addEvent.java
+++ src/main/java/com/example/PiDev/addEvent.java
@@ -11,6 +11,8 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.time.LocalDate;
+
 public class addEvent {
 
     @FXML
@@ -36,15 +38,103 @@ public class addEvent {
     @FXML
     void saveEvent() {
         DBHelper dbHelper = new DBHelper();
+        // Validate event name
+        String eventName = etnom.getText().trim();
+        if (eventName.isEmpty()) {
+            // Show error message
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter event name");
+            alert.showAndWait();
+            return;
+        }
+
+// Validate event capacity
+        int capacity = 0;
+        try {
+            capacity = Integer.parseInt(etcapacite.getText().trim());
+            if (capacity <= 0) {
+                // Show error message
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter a positive integer for capacity");
+                alert.showAndWait();
+                return;
+            }
+        } catch (NumberFormatException e) {
+            // Show error message
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter a valid integer for capacity");
+            alert.showAndWait();
+            return;
+        }
+
+// Validate event location
+        String location = local.getText().trim();
+        if (location.isEmpty()) {
+            // Show error message
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter event location");
+            alert.showAndWait();
+            return;
+        }
+
+// Validate event date
+        LocalDate eventDate = date.getValue();
+        if (eventDate == null) {
+            // Show error message
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please select event date");
+            alert.showAndWait();
+            return;
+        }
+
+// Validate event price
+        double price = 0.0;
+        try {
+            price = Double.parseDouble(prix.getText().trim());
+            if (price <= 0.0) {
+                // Show error message
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter a positive number for price");
+                alert.showAndWait();
+                return;
+            }
+        } catch (NumberFormatException e) {
+            // Show error message
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter a valid number for price");
+            alert.showAndWait();
+            return;
+        }
+
+// Validate event description
+        String description = desc.getText().trim();
+        if (description.isEmpty()) {
+            // Show error message
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter event description");
+            alert.showAndWait();
+            return;
+        }
+
+// Validate event category
+        String category = categorie.getText().trim();
+        if (category.isEmpty()) {
+            // Show error message
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Please enter event category");
+            alert.showAndWait();
+            return;
+        }
+
+// If all input is valid, create event object and save to database
         Event e = new Event(
-        1,
-                etnom.getText(),
-                Integer.parseInt(etcapacite.getText()),
-                local.getText(),
-                date.getValue().toString(),
-                Double.parseDouble(prix.getText()),
-                desc.getText(), categorie.getText());
-        dbHelper.saveEvent( e) ;
+                1,
+                eventName,
+                capacity,
+                location,
+                eventDate.toString(),
+                price,
+                description,
+                category);
+
+        dbHelper.saveEvent(e);
+
+// Show success message
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, "Event saved successfully");
+        alert.showAndWait();
+
+
         etnom.clear();
         etcapacite.clear();
         local.clear();
@@ -59,7 +149,7 @@ public class addEvent {
         Button closeButton = new Button("OK");
         closeButton.setOnAction(closeEvent -> dialogStage.close());
         dialogBox.getChildren().addAll(dialogLabel, closeButton);
-        Scene dialogScene = new Scene(dialogBox, 200, 100);
+        Scene dialogScene = new Scene(dialogBox, 100, 100);
         dialogStage.setScene(dialogScene);
         dialogStage.show();
 
